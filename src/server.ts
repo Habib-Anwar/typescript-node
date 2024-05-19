693c8ad380d94e615f0c91607f4c3d28f675bdb8
unknown <tauft@aol.com>
2024-05-19 20:48:40 +0600

    basic project setup

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
+
+
+async function main() {
+    try {
+
+        await mongoose.connect(config.database_url as string);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.database_url}`)
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+  
+ 
+  }
+
+
+
+
